e156b29bd73ff4e03ae700e635d629ee14cb2e90
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2023-11-03 11:26:20 +0200

    added tsk-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector('.gallery');
 
-console.log(galleryItems);
+
+const gallery = galleryItems.map(({preview, original, description}) => {
+    return `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+    `
+}).join('');
+
+galleryList.insertAdjacentHTML('beforeend', gallery);
+
+var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
